c97a047b515df9b95015fe9515038ed319477117
marchalgreen <marchalgreen@gmail.com>
2025-11-04 13:30:51 +0100

    chore: remove inventory and vendor-sync dependent modules

--- packages/web/components/windows/InventoryDashboardWindow.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import InventoryDashboard from '@/components/inventory/Dashboard/InventoryDashboard';
-
-export default function InventoryDashboardWindow() {
-  return <InventoryDashboard />;
-}
