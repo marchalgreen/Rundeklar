d19f9f5020842e646ef35e2722c4fa1fd56fe75f
marchalgreen <marchalgreen@gmail.com>
2025-12-20 23:24:25 +0100

    feat(statistics): add PlayerOpponentList component for expandable head-to-head stats
    - Create PlayerOpponentList component to display top partners/opponents
    - Add expandable sections to show head-to-head statistics when clicking on players
    - Integrate with head-to-head data loading functionality
    - Export component from statistics index

--- packages/webapp/src/components/statistics/index.ts
+++ packages/webapp/src/components/statistics/index.ts
@@ -1,5 +1,6 @@
 export { RecentMatches } from './RecentMatches'
 export { HeadToHeadResults } from './HeadToHeadResults'
+export { PlayerOpponentList } from './PlayerOpponentList'
 export { StatisticsLanding } from './StatisticsLanding'
 export { StatisticsFilters } from './StatisticsFilters'
 export { KPICards } from './KPICards'
